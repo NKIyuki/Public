1e7a62716549bb9a454e00ea5dfa6037dbe95097
NKIyuki <u.y0701@icloud.com>
2023-02-18 10:07:46 +0000

    マイグレート

--- db/migrate/20230210091713_create_items.rb
+++ db/migrate/20230210091713_create_items.rb
@@ -3,10 +3,10 @@ class CreateItems < ActiveRecord::Migration[6.1]
     create_table :items do |t|
       t.integer :genre_id,null:false
       t.string :name,null:false
-   　　t.text :introduction,null:false
-   　　t.integer :price,null:false
-   　　t.boolean :is_active,null:false
-   　　t.timestamps
+      t.text :introduction,null:false
+      t.integer :price,null:false
+      t.boolean :is_active,null:false
+      t.timestamps
     end
   end
 end
