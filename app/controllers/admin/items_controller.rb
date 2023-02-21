e8497c0767c0f01bbd04856d0db8485f9697f436
NKIyuki <u.y0701@icloud.com>
2023-02-21 12:22:22 +0000

    item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,24 +8,32 @@ class Admin::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @items = @genre
 
   end
 
   def index
-　　@items = Item.all
+    @items = Item.all
+    @item = Genre.all
   end
 
   def edit
-    @items = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
+  end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
 
   private
   def item_params
-    params.require(:item).permit(:name,:introduction,:price)
+    params.require(:item).permit(:name,:introduction,:price,:genre_id,:is_active,:image)
   end
 end
