16b901d2524f4ce5c433a49b89d39f5664cc5bce
NKIyuki <u.y0701@icloud.com>
2023-02-22 12:33:57 +0000

    cusomers

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,15 +5,22 @@ class Admin::CustomersController < ApplicationController
 
   def index
     @customers = Customer.all
+
   end
 
   def edit
-   @customers = Customer.find(params[:id])
+    @custome = Customer.find(params[:id])
 
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer.id)
+  end
 
-
+  private
+  def customer_params
+    params.require(:customer).permit(:email,:is_deleted,:last_name,:first_name,:first_name_kana,:last_name_kana,:postal_code,:address,:telephone_number)
   end
 end
