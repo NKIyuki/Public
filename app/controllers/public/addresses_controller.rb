8dc6261c6d9f37e7ea2cbc42a5cc8ccf092a2ac3
NKIyuki <u.y0701@icloud.com>
2023-02-24 12:53:17 +0000

    addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,3 +1,9 @@
 class Public::AddressesController < ApplicationController
- 
+  def index
+     @addresses = Addresses.all
+  end
+
+  def edit
+    @addresses = Addresses.faind(params[:id])
+  end
 end
