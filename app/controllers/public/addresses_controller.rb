eebda5ec0d8c13ae77235f5ccaff8c6437839534
NKIyuki <u.y0701@icloud.com>
2023-02-25 13:06:32 +0000

    address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,9 +1,29 @@
 class Public::AddressesController < ApplicationController
   def index
-     @addresses = Addresses.all
+     @address = Address.all
   end
 
   def edit
-    @addresses = Addresses.faind(params[:id])
+    @address = Address.find(params[:id])
   end
+
+  def create
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    address.save
+    redirect_to public_addresses_path
+  end
+
+  def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to public_addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:name,:postal_code,:address)
+  end
+
+
 end
